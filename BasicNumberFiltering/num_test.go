69bf3b703d2978bb39310b18b80e8b20d7dd3165
Nilesh D <42713643+Nilesh2000@users.noreply.github.com>
2024-10-09 03:27:20 +0530

    test(num_test.go): refactor test assertions to use assertSliceEquality function for improved readability and maintainability fix(num_test.go): correct function name from TestOddAndMultipleOfThreeAndGreaterThanTe to TestOddAndMultipleOfThreeAndGreaterThanTen for clarity

--- BasicNumberFiltering/num_test.go
+++ BasicNumberFiltering/num_test.go
@@ -9,51 +9,46 @@ func TestEven(t *testing.T) {
 	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	got := Even(numbers)
 	want := []int{2, 4, 6, 8, 10}
-	if !slices.Equal(got, want) {
-		t.Errorf("got %v want %v", got, want)
-	}
+	assertSliceEquality(t, got, want)
 }
 
 func TestOdd(t *testing.T) {
 	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	got := Odd(numbers)
 	want := []int{1, 3, 5, 7, 9}
-	if !slices.Equal(got, want) {
-		t.Errorf("got %d want %v", got, want)
-	}
+	assertSliceEquality(t, got, want)
 }
 
 func TestPrime(t *testing.T) {
 	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	got := Prime(numbers)
 	want := []int{2, 3, 5, 7}
-	if !slices.Equal(got, want) {
-		t.Errorf("got %v want %v", got, want)
-	}
+	assertSliceEquality(t, got, want)
 }
 
 func TestPrimeAndOdd(t *testing.T) {
 	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	got := PrimeAndOdd(numbers)
 	want := []int{3, 5, 7}
-	if !slices.Equal(got, want) {
-		t.Errorf("got %v want %v", got, want)
-	}
+	assertSliceEquality(t, got, want)
 }
 
 func TestEvenAndMultipleOfFive(t *testing.T) {
 	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
 	got := EvenAndMultipleOfFive(numbers)
 	want := []int{10, 20}
-	if !slices.Equal(got, want) {
-		t.Errorf("got %v want %v", got, want)
-	}
+	assertSliceEquality(t, got, want)
 }
 
-func TestOddAndMultipleOfThreeAndGreaterThanTe(t *testing.T) {
+func TestOddAndMultipleOfThreeAndGreaterThanTen(t *testing.T) {
 	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
 	got := OddAndMultipleOfThreeAndGreaterThanTen(numbers)
 	want := []int{15}
+	assertSliceEquality(t, got, want)
+}
+
+func assertSliceEquality(t testing.TB, got, want []int) {
+	t.Helper()
 	if !slices.Equal(got, want) {
 		t.Errorf("got %v want %v", got, want)
 	}
